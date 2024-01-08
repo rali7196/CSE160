df17693fc7fe8ae89afb3a09768c86cbe785960d
Rehan Ali <rali3@ucsc.edu>
2024-01-08 11:22:40 -0800

    initial commit

---
+++ prog_0/driver.js
@@ -0,0 +1,5 @@
+// Your JavaScript code for canvas interaction goes here
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+
+// Add your canvas drawing or interaction code here
