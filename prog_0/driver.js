0a1870205876126d75bd8887af485e275f117be7
Rehan Ali <rali3@ucsc.edu>
2024-01-08 16:19:18 -0800

    finished line drawin g logic

--- prog_0/driver.js
+++ prog_0/driver.js
@@ -2,4 +2,35 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
+canvas.addEventListener('click', drawPoint)
+ctx.beginPath();
+point_counter = 1
+
+function drawPoint(){
+    let rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+    console.log("Coordinate x: " + x)
+    console.log("Coordinate y: " + y);
+    console.log("point_counter: " + point_counter)
+    if(point_counter == 1){
+        console.log("first success")
+        ctx.moveTo(x, y);
+        point_counter = point_counter + 1
+    }
+    else if(point_counter % 2 == 0){
+        console.log("second success")
+        ctx.lineTo(x, y);
+        ctx.stroke();
+        ctx.moveTo(x,y)
+    }
+
+
+    // console.log("Success")
+
+
+}
+
+
+
 // Add your canvas drawing or interaction code here
